cb49fbcbfe6815a104ca0e1e58deeb1a3feabfb0
Kurt metzelfeld <kmetz73@gmail.com>
2025-02-28 12:02:06 -0600

    started implementing book UI on admin page

--- components/admin/Header.tsx
+++ components/admin/Header.tsx
@@ -8,7 +8,7 @@ const Header = ({ session }: { session: Session }) => {
           {session?.user?.name}
         </h2>
         <p className="text-base text-slate-500">
-          Monitor all of your users and book here
+          Monitor all of your users and books here
         </p>
       </div>
       <p>Search</p>
