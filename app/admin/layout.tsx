c63fa8210f338b2b4b57bf8bb70eb360ac083989
Kurt metzelfeld <kmetz73@gmail.com>
2025-03-04 11:18:12 -0600

    implemented  action to  add new book to DB and added on  ADMIN  can  go to  admin page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,11 +5,23 @@ import { ReactNode } from 'react';
 import '@/styles/admin.css';
 import Sidebar from '@/components/admin/Sidebar';
 import Header from '@/components/admin/Header';
+import { db } from '@/database/drizzle';
+import { eq } from 'drizzle-orm';
+import { users } from '@/database/schema';
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session?.user?.id) redirect('/sign-in');
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === 'ADMIN');
+
+  if (!isAdmin) redirect('/');
 
   return (
     <main className="bg-slate-200! flex min-h-screen w-full flex-row">
--- components/admin/forms/BookForm.tsx
+++ components/admin/forms/BookForm.tsx
@@ -25,7 +25,8 @@ import { toast } from '@/hooks/use-toast';
 import { Textarea } from '@/components/ui/textarea';
 import FileUpload from '@/components/FileUpload';
 import ColorPicker from '../ColorPicker';
-
+import { createBook } from '@/lib/admin/actions/book';
+import { ToastViewport } from '@/components/ui/toast';
 interface Props extends Partial<Book> {
   type?: 'create' | 'update';
 }
@@ -50,7 +51,24 @@ const BookForm = ({ type, ...book }: Props) => {
   });
 
   const onSubmit = async (values: z.infer<typeof bookSchema>) => {
-    console.log(values);
+    // console.log(values);
+
+    const result = await createBook(values);
+
+    if (result.success) {
+      toast({
+        title: 'Success',
+        description: 'Book created successfully.',
+      });
+
+      router.push(`/admin/books/${result.data.id}`);
+    } else {
+      toast({
+        title: 'Error',
+        description: result.message,
+        variant: 'destructive',
+      });
+    }
   };
 
   return (
